f49e0f72450057b602fd0332bdbd221dc3b9e98a
Vadim <electoratium@gmail.com>
2018-02-19 17:01:23 +0500

    work on animation connection block

--- static/static_dev/js/pages_script/training_script.js
+++ static/static_dev/js/pages_script/training_script.js
@@ -3,168 +3,173 @@
 document.addEventListener("DOMContentLoaded", function() {
     var name_form = document.querySelector('.form');
 
+    function connection_definition() {
 
 
+        var accept_el = {
+            id: '#drop-1,#drop-2,#drop-3,#drop-4'
+        };
+        var correct_filled_block = 0;
 
-    var accept_el = {
-        id: '#drop-1,#drop-2,#drop-3,#drop-4'
-    };
-
-    interact('.dropzone').dropzone({
-      // only accept elements matching this CSS selector
-      accept: accept_el['id'],
-
-      // Require a 25% element overlap for a drop to be possible
-      overlap: 0.25,
-      // checker: function (dragEvent, event, dropped, dropzone, dropEl, draggable, draggableElement) {
-      //     var nmb_question = draggableElement.getAttribute('data-definition');
-      //     var nmb_dropzone = dropEl.getAttribute('data-dropzone');
-      //
-      //
-      //     console.log(nmb_dropzone);
-      //     console.log(nmb_question);
-      //
-      //
-      //     return true
-      //   // return dropped && !dropElement.hasChildNodes();
-      // },
-
-      // listen for drop related events:
-      ondropactivate: function (event) {
-        // add active dropzone feedback
-        event.target.classList.add('drop-active');
-      },
-      ondragenter: function (event) {
-
-        var draggableElement = event.relatedTarget,
-            dropzoneElement = event.target;
+        //html el
+        var progress = document.querySelector('#connection_definition .progress_task');
+        var condition = document.querySelector('#connection_definition .condition');
+        var concepts_block = document.querySelector('#connection_definition #concepts_block');
+        var dropzones_block = document.querySelector('#connection_definition #dropzones_block');
+        var definition_block = document.querySelector('#connection_definition #definition_block');
+        var button = document.querySelector('#connection_definition .next-test-btn');
 
 
+        interact('.dropzone').dropzone({
+            // only accept elements matching this CSS selector
+            accept: accept_el['id'],
+            // Require a 25% element overlap for a drop to be possible
+            overlap: 0.25,
+            // listen for drop related events:
+            ondropactivate: function (event) {
+                event.target.classList.add('drop-active');
+            },
+            ondragenter: function (event) {
+                var draggableElement = event.relatedTarget,
+                    dropzoneElement = event.target;
+
+                if (validate_position(draggableElement, dropzoneElement)) {
+                    // feedback the possibility of a drop
+                    dropzoneElement.classList.add('drop-target');
+                    // draggableElement.classList.add('can-drop');
+                }
+                else {
+                    dropzoneElement.classList.add('cant-drop');
+                }
+            },
+            ondragleave: function (event) {
+                event.target.classList.remove('drop-target');
+                event.relatedTarget.classList.remove('can-drop');
+                event.target.classList.remove('cant-drop');
+            },
+            ondrop: function (event) {
+                var draggableElement = event.relatedTarget,
+                    dropzoneElement = event.target;
+
+                if (validate_position(draggableElement, dropzoneElement)) {
+                    correct_filled_block++;
+                    remove_drag_element(draggableElement, dropzoneElement);
+
+                    align_element(dropzoneElement, draggableElement);
+
+                    if (correct_filled_block == 4) {
+                        show_button();
+                    }
+                }
+                else {
+                    dropzoneElement.classList.remove('cant-drop');
+
+                    draggableElement.style.transform =
+                        'translate( 0, 0)';
+                    draggableElement.setAttribute('data-x', 0);
+                    draggableElement.setAttribute('data-y', 0);
+                }
+            },
+            ondropdeactivate: function (event) {
+                // remove active dropzone feedback
+                event.target.classList.remove('drop-active');
+                event.target.classList.remove('drop-target');
+            }
+        });
+        interact('.draggable').draggable({
+
+            // enable inertial throwing
+            inertia: true,
+            // keep the element within the area of it's parent
+            restrict: {
+                restriction: ".connection_block",
+                endOnly: true,
+                elementRect: {top: 0, left: 0, bottom: 1, right: 1}
+            },
+            // enable autoScroll
+            autoScroll: true,
+            // call this function on every dragmove event
+            onmove: dragMoveListener
+        });
 
 
+        function validate_position(draggable_el, dropzone_el) {
+            var nmb_question = draggable_el.getAttribute('data-definition'),
+                nmb_dropzone = dropzone_el.getAttribute('data-dropzone');
 
-        if (validate_position(draggableElement, dropzoneElement)) {
-            // feedback the possibility of a drop
-            dropzoneElement.classList.add('drop-target');
-            draggableElement.classList.add('can-drop');
-
-            // var height_drag_el = parseInt((getComputedStyle(draggableElement).height).slice(0,-2)) + 20;
-            // dropzoneElement.style.height = height_drag_el + 'px';
+            if (nmb_question === nmb_dropzone) {
+                return true
+            }
+            return false
         }
-        else{
-            dropzoneElement.classList.add('cant-drop');
+
+        function remove_drag_element(draggable_el, dropzone) {
+            draggable_el.classList.remove('draggable');
+            dropzone.classList.remove('dropzone');
         }
-      },
-      ondragleave: function (event) {
-        // remove the drop feedback style
-        event.target.classList.remove('drop-target');
-        event.relatedTarget.classList.remove('can-drop');
-        event.target.classList.remove('cant-drop');
-      },
-      ondrop: function (event) {
-          var draggableElement = event.relatedTarget,
-            dropzoneElement = event.target;
 
-          if(validate_position(draggableElement, dropzoneElement)){
-              remove_drag_eleement(draggableElement, dropzoneElement);
+        function align_element(dropzone, draggable_el) {
+            var box_dropzone = dropzone.getBoundingClientRect(),
+                box_draggable_el = draggable_el.getBoundingClientRect();
 
-              // align_element(dropzoneElement, draggableElement);
-              
-          }
-          else{
+            draggable_el.style.left = (box_dropzone.left - box_draggable_el.left) + 'px';
+            draggable_el.style.top = (box_dropzone.top - box_draggable_el.top) + 'px';
+        }
 
-          }
-      },
-      ondropdeactivate: function (event) {
-        // remove active dropzone feedback
+        function dragMoveListener(event) {
+            var target = event.target,
+                // keep the dragged position in the data-x/data-y attributes
+                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-        event.target.classList.remove('drop-active');
-        event.target.classList.remove('drop-target');
-      }
-    });
-    interact('.draggable').draggable({
+            // translate the element
+            target.style.webkitTransform =
+                target.style.transform =
+                    'translate(' + x + 'px, ' + y + 'px)';
 
-    // enable inertial throwing
-    inertia: true,
-    // keep the element within the area of it's parent
-    // restrict: {
-    //   restriction: "parent",
-    //   endOnly: true,
-    //   elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    // },
-    // enable autoScroll
-    autoScroll: true,
+            // update the posiion attributes
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        }
 
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-
-    // вызов ф-и когда опустил элемент
-    // onend: function (event) {
-    //
-    // }
-    });
-    
-    function validate_position(draggable_el, dropzone_el) {
-         var nmb_question = draggable_el.getAttribute('data-definition'),
-            nmb_dropzone = dropzone_el.getAttribute('data-dropzone');
+        function show_button() {
+            button.classList.toggle('hide_element');
 
 
-         if (nmb_question === nmb_dropzone ){
-             return true
-         }
-         return false
+            // button.addEventListener('click', next_test);
+        }
+
+
+        function show_connection_block() {
+            //show title block
+            progress.classList.add('show_element');
+            condition.classList.add('show_element');
+
+            //show connection_definition block
+            concepts_block.style.left = '0';
+            dropzones_block.style.opacity = '1';
+            definition_block.style.right = '0';
+        }
+
+
+
+        setTimeout(show_connection_block, 1000);
+
+        function hide_connection_block() {
+            progress.classList.remove('show_element');
+            condition.classList.remove('show_element');
+
+            //show connection_definition block
+            concepts_block.style.left = '-100%';
+            dropzones_block.style.opacity = '0';
+            definition_block.style.right = '-100%';
+        }
+
+        setTimeout(hide_connection_block, 3000)
+
+
     }
-
-    function remove_drag_eleement(draggable_el, dropzone) {
-        draggable_el.classList.remove('draggable');
-        dropzone.classList.remove('dropzone');
-    }
-    // function align_element(dropzone, draggable_el) {
-    //     var box_dropzone = dropzone.getBoundingClientRect(),
-    //         box_draggable_el =  draggable_el.getBoundingClientRect();
-    //     var dropzone_coords = {
-    //         box: dropzone.getBoundingClientRect(),
-    //         coords:{
-    //             top: box_dropzone.top,
-    //             left: box_dropzone.left
-    //         }
-    //     };
-    //
-    //     var draggable_el_coords = {
-    //         coords: {
-    //             top: box_draggable_el.top,
-    //             left: box_draggable_el.left
-    //         }
-    //     };
-    //
-    //
-    //
-    // }
-    
-    
-    
-    function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
-    // window.dragMoveListener = dragMoveListener;
-
-
-
-
-
-
+    connection_definition();
 
 
 
@@ -194,14 +199,13 @@ document.addEventListener("DOMContentLoaded", function() {
                     var nmb_correct_test = 0;
 
                     //html el
-                    var completed_tests_container = document.querySelector('#progress');
-                    var condition_container = document.querySelector('#condition_test');
+                    var completed_tests_container = document.querySelector('#test_block .progress_task');
+                    var condition_container = document.querySelector('#test_block .condition');
                     var test_block = document.querySelector('#test_block .list-group');
                     var answer_elements = document.querySelectorAll('#test_block .list-group a');
-                    var completed_test = document.querySelector('#progress p');
-                    var condition = document.querySelector('#condition_test h3');
-                    var button = document.querySelector('.next-test-btn');
-
+                    var completed_test = document.querySelector('#test_block .progress_task p');
+                    var condition = document.querySelector('.condition h3');
+                    var button = document.querySelector('#test_block .next-test-btn');
 
 
 
