7b6e12d03074ac95c1059686b8ea9456b75aacf8
Vadim <electoratium@gmail.com>
2018-02-20 18:05:04 +0500

    finished connection definition and start develop exercises

--- static/static_dev/js/pages_script/training_script.js
+++ static/static_dev/js/pages_script/training_script.js
@@ -3,175 +3,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var name_form = document.querySelector('.form');
 
-    function connection_definition() {
-
-
-        var accept_el = {
-            id: '#drop-1,#drop-2,#drop-3,#drop-4'
-        };
-        var correct_filled_block = 0;
-
-        //html el
-        var progress = document.querySelector('#connection_definition .progress_task');
-        var condition = document.querySelector('#connection_definition .condition');
-        var concepts_block = document.querySelector('#connection_definition #concepts_block');
-        var dropzones_block = document.querySelector('#connection_definition #dropzones_block');
-        var definition_block = document.querySelector('#connection_definition #definition_block');
-        var button = document.querySelector('#connection_definition .next-test-btn');
-
-
-        interact('.dropzone').dropzone({
-            // only accept elements matching this CSS selector
-            accept: accept_el['id'],
-            // Require a 25% element overlap for a drop to be possible
-            overlap: 0.25,
-            // listen for drop related events:
-            ondropactivate: function (event) {
-                event.target.classList.add('drop-active');
-            },
-            ondragenter: function (event) {
-                var draggableElement = event.relatedTarget,
-                    dropzoneElement = event.target;
-
-                if (validate_position(draggableElement, dropzoneElement)) {
-                    // feedback the possibility of a drop
-                    dropzoneElement.classList.add('drop-target');
-                    // draggableElement.classList.add('can-drop');
-                }
-                else {
-                    dropzoneElement.classList.add('cant-drop');
-                }
-            },
-            ondragleave: function (event) {
-                event.target.classList.remove('drop-target');
-                event.relatedTarget.classList.remove('can-drop');
-                event.target.classList.remove('cant-drop');
-            },
-            ondrop: function (event) {
-                var draggableElement = event.relatedTarget,
-                    dropzoneElement = event.target;
-
-                if (validate_position(draggableElement, dropzoneElement)) {
-                    correct_filled_block++;
-                    remove_drag_element(draggableElement, dropzoneElement);
-
-                    align_element(dropzoneElement, draggableElement);
-
-                    if (correct_filled_block == 4) {
-                        show_button();
-                    }
-                }
-                else {
-                    dropzoneElement.classList.remove('cant-drop');
-
-                    draggableElement.style.transform =
-                        'translate( 0, 0)';
-                    draggableElement.setAttribute('data-x', 0);
-                    draggableElement.setAttribute('data-y', 0);
-                }
-            },
-            ondropdeactivate: function (event) {
-                // remove active dropzone feedback
-                event.target.classList.remove('drop-active');
-                event.target.classList.remove('drop-target');
-            }
-        });
-        interact('.draggable').draggable({
-
-            // enable inertial throwing
-            inertia: true,
-            // keep the element within the area of it's parent
-            restrict: {
-                restriction: ".connection_block",
-                endOnly: true,
-                elementRect: {top: 0, left: 0, bottom: 1, right: 1}
-            },
-            // enable autoScroll
-            autoScroll: true,
-            // call this function on every dragmove event
-            onmove: dragMoveListener
-        });
-
-
-        function validate_position(draggable_el, dropzone_el) {
-            var nmb_question = draggable_el.getAttribute('data-definition'),
-                nmb_dropzone = dropzone_el.getAttribute('data-dropzone');
-
-            if (nmb_question === nmb_dropzone) {
-                return true
-            }
-            return false
-        }
-
-        function remove_drag_element(draggable_el, dropzone) {
-            draggable_el.classList.remove('draggable');
-            dropzone.classList.remove('dropzone');
-        }
-
-        function align_element(dropzone, draggable_el) {
-            var box_dropzone = dropzone.getBoundingClientRect(),
-                box_draggable_el = draggable_el.getBoundingClientRect();
-
-            draggable_el.style.left = (box_dropzone.left - box_draggable_el.left) + 'px';
-            draggable_el.style.top = (box_dropzone.top - box_draggable_el.top) + 'px';
-        }
-
-        function dragMoveListener(event) {
-            var target = event.target,
-                // keep the dragged position in the data-x/data-y attributes
-                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-            // translate the element
-            target.style.webkitTransform =
-                target.style.transform =
-                    'translate(' + x + 'px, ' + y + 'px)';
-
-            // update the posiion attributes
-            target.setAttribute('data-x', x);
-            target.setAttribute('data-y', y);
-        }
-
-        function show_button() {
-            button.classList.toggle('hide_element');
-
-
-            // button.addEventListener('click', next_test);
-        }
-
-
-        function show_connection_block() {
-            //show title block
-            progress.classList.add('show_element');
-            condition.classList.add('show_element');
-
-            //show connection_definition block
-            concepts_block.style.left = '0';
-            dropzones_block.style.opacity = '1';
-            definition_block.style.right = '0';
-        }
-
-
-
-        setTimeout(show_connection_block, 1000);
-
-        function hide_connection_block() {
-            progress.classList.remove('show_element');
-            condition.classList.remove('show_element');
-
-            //show connection_definition block
-            concepts_block.style.left = '-100%';
-            dropzones_block.style.opacity = '0';
-            definition_block.style.right = '-100%';
-        }
-
-        setTimeout(hide_connection_block, 3000)
-
-
-    }
-    connection_definition();
-
-
 
     name_form.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -310,10 +141,245 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
                 }
 
-                // test_block()
+                // test_block();
+
+                function connection_definition() {
+                    var connection_definition_data = data['connections_definition'];
+                    var concepts = connection_definition_data['concepts'];
+                    var definitions = connection_definition_data['definitions'];
+
+                    var current_connection = 0;
+                    var nmb_connections = concepts.length;
+                    var accept_el = {
+                        id: '#drop-1,#drop-2,#drop-3,#drop-4'
+                    };
+                    var correct_filled_block = {
+                        value: 0
+                    };
+
+                    //html el
+                    var connection_block = document.querySelector('#connection_definition');
+                    var progress = document.querySelector('#connection_definition .progress_task');
+                    var condition = document.querySelector('#connection_definition .condition');
+                    var concepts_block = document.querySelector('#connection_definition #concepts_block');
+                    var concepts_elements = document.querySelectorAll('#connection_definition #concepts_block li');
+                    var dropzones_block = document.querySelector('#connection_definition #dropzones_block');
+                    var dropzones_elements = document.querySelectorAll('#connection_definition #dropzones_block li');
+                    var definition_block = document.querySelector('#connection_definition #definition_block');
+                    var definition_elements = document.querySelectorAll('#connection_definition #definition_block li');
+                    var button = document.querySelector('#connection_definition .next-test-btn');
 
 
+                    connection_block.classList.remove('delete_element');
 
+
+                    interact('.dropzone').dropzone({
+                        // only accept elements matching this CSS selector
+                        accept: accept_el['id'],
+                        // Require a 25% element overlap for a drop to be possible
+                        overlap: 0.25,
+                        // listen for drop related events:
+                        ondropactivate: function (event) {
+                            event.target.classList.add('drop-active');
+                        },
+                        ondragenter: function (event) {
+                            var draggableElement = event.relatedTarget,
+                                dropzoneElement = event.target;
+
+                            if (validate_position(draggableElement, dropzoneElement)) {
+                                // feedback the possibility of a drop
+                                dropzoneElement.classList.add('drop-target');
+                            }
+                            else {
+                                dropzoneElement.classList.add('cant-drop');
+                            }
+                        },
+                        ondragleave: function (event) {
+                            event.target.classList.remove('drop-target');
+                            event.relatedTarget.classList.remove('can-drop');
+                            event.target.classList.remove('cant-drop');
+                        },
+                        ondrop: function (event) {
+                            var draggableElement = event.relatedTarget,
+                                dropzoneElement = event.target;
+
+                            if (validate_position(draggableElement, dropzoneElement)) {
+                                correct_filled_block['value']++;
+                                remove_drag_element(draggableElement, dropzoneElement);
+
+                                align_element(dropzoneElement, draggableElement);
+
+                                if (correct_filled_block['value'] === 4) {
+                                    show_button();
+                                }
+                            }
+                            else {
+                                dropzoneElement.classList.remove('cant-drop');
+
+                                draggableElement.style.transform =
+                                    'translate( 0, 0)';
+                                draggableElement.setAttribute('data-x', 0);
+                                draggableElement.setAttribute('data-y', 0);
+                            }
+                        },
+                        ondropdeactivate: function (event) {
+                            // remove active dropzone feedback
+                            event.target.classList.remove('drop-active');
+                            event.target.classList.remove('drop-target');
+                        }
+                    });
+                    interact('.draggable').draggable({
+                        // enable inertial throwing
+                        inertia: true,
+                        // keep the element within the area of it's parent
+                        restrict: {
+                            restriction: ".connection_block",
+                            endOnly: true,
+                            elementRect: {top: 0, left: 0, bottom: 1, right: 1}
+                        },
+                        // enable autoScroll
+                        autoScroll: true,
+                        // call this function on every dragmove event
+                        onmove: dragMoveListener
+                    });
+
+                    function validate_position(draggable_el, dropzone_el) {
+                        var nmb_question = draggable_el.getAttribute('data-definition'),
+                            nmb_dropzone = dropzone_el.getAttribute('data-dropzone');
+
+                        if (nmb_question === nmb_dropzone) {
+                            return true
+                        }
+                        return false
+                    }
+
+                    function remove_drag_element(draggable_el, dropzone) {
+                        draggable_el.classList.remove('draggable');
+                        dropzone.classList.remove('dropzone');
+                    }
+
+                    function align_element(dropzone, draggable_el) {
+                        var box_dropzone = dropzone.getBoundingClientRect(),
+                            box_draggable_el = draggable_el.getBoundingClientRect();
+
+                        draggable_el.style.left = (box_dropzone.left - box_draggable_el.left) + 'px';
+                        draggable_el.style.top = (box_dropzone.top - box_draggable_el.top) + 'px';
+                    }
+
+                    function dragMoveListener(event) {
+                        var target = event.target,
+                            // keep the dragged position in the data-x/data-y attributes
+                            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+                        // translate the element
+                        target.style.webkitTransform =
+                            target.style.transform =
+                                'translate(' + x + 'px, ' + y + 'px)';
+
+                        // update the posiion attributes
+                        target.setAttribute('data-x', x);
+                        target.setAttribute('data-y', y);
+                    }
+
+                    function show_button() {
+                        button.classList.remove('hide_element');
+
+
+                        button.addEventListener('click', next_task);
+                    }
+
+                    function next_task() {
+                        current_connection++;
+
+                        correct_filled_block['value'] = 0;
+
+                        var current_task = current_connection + 1;
+
+                        hide_connection_block();
+                        //hide button
+                        button.classList.add('hide_element');
+                        if(current_task <= nmb_connections) {
+                            setTimeout(restart_connection, 1000);
+                            setTimeout(fill_elements, 1000);
+                            setTimeout(show_connection_block, 1000);
+                        }
+                        else{
+                            setTimeout(function(){
+                                connection_block.classList.add('delete_element')
+                            }, 1000);
+                        }
+                    }
+
+                    function show_connection_block() {
+                        //show title block
+                        progress.classList.add('show_element');
+                        condition.classList.add('show_element');
+
+                        //show connection_definition block
+                        concepts_block.style.left = '0';
+                        dropzones_block.style.opacity = '1';
+                        definition_block.style.right = '0';
+                    }
+                    setTimeout(show_connection_block, 500);
+
+                    function fill_elements() {
+                        document.querySelector('#connection_definition .progress_task p').innerHTML = "Завдання: " + (current_connection + 1) + '/' + nmb_connections;
+
+                        var keys = [];
+                        for(var item in definitions[current_connection]){
+                            keys.push(item);
+                        }
+                        var answers_id = [];
+                        for(var key of keys){
+                            answers_id.push(key.slice(-1));
+                        }
+
+                        for (var i = 0;i < 4; i++){
+                            concepts_elements[i].innerHTML = concepts[current_connection][i];
+
+                            definition_elements[i].innerHTML = definitions[current_connection][keys[i]];
+
+                            definition_elements[i].setAttribute('data-definition', answers_id[i]);
+                        }
+
+                    }
+                    //first activate
+                    fill_elements();
+
+                    function restart_connection() {
+
+                        for(var draggable_el of definition_elements) {
+                            draggable_el.style.transform =
+                                'translate( 0, 0)';
+
+                            //move to start position dtaggable element
+                            draggable_el.setAttribute('data-x', 0);
+                            draggable_el.setAttribute('data-y', 0);
+                            draggable_el.style.left = 0;
+                            draggable_el.style.top = 0;
+
+                            //activate draggable element
+                            draggable_el.classList.add('draggable');
+                        }
+
+                        //activate dropzones
+                        for(var dropzone of dropzones_elements){
+                            dropzone.classList.add('dropzone');
+                        }
+                    }
+
+                    function hide_connection_block() {
+                        progress.classList.remove('show_element');
+                        condition.classList.remove('show_element');
+
+                        //show connection_definition block
+                        concepts_block.style.left = '-100%';
+                        dropzones_block.style.opacity = '0';
+                        definition_block.style.right = '-100%';
+                    }
+                }
+                // setTimeout(connection_definition, 1000);
 
 
 
