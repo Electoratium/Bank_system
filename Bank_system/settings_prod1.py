a8004c04159dd29124e61048717fa67bcd486752
Vadim <electoratium@gmail.com>
2018-02-27 15:35:55 +0300

    change initial_data.json

--- Bank_system/settings_prod1.py
+++ Bank_system/settings_prod1.py
@@ -5,9 +5,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1',
-        'USER' : 'bank_system',
-        'PASSWORD' : 'bank_system',
-        'HOST' : 'localhost',
-        'PORT' : '',
+        'USER': 'bank_system',
+        'PASSWORD': 'bank_system',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
